135ddd9a833bd1e7f695b2cb11f236b91be55a50
CusanoTech <programmingcus@gmail.com>
2023-07-25 19:59:12 -0300

    Terminamos el nav y creamos animacion scrum con JS

---
+++ Front/main.js
@@ -0,0 +1,14 @@
+
+const scrum = new Typed('.scrum', {
+    strings: [
+        'FULL STACK DEVELOPER',
+        'WITH EXPERTISE IN PYTHON'
+    ],
+    loop: true,         //repetir array de strings
+    backDelay: 1200,    // tempo al termina cada palabra
+    typeSpeed: 70,
+    backSpeed: 80,      // velocidad en eliminar palabras
+    showCursor: true,   //cursor palpitando
+    cursorChar: '|'
+});
+
